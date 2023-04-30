6ae593b3b4cea96e1be40d7cefbbd5084f765317
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-04-30 20:20:08 +0200

    added employeeName component

--- src/components/SixWeekComponent.jsx
+++ src/components/SixWeekComponent.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import WeekComponent from './WeekComponent'
 import styled from 'styled-components';
 
-export default function SixWeekComponent({week}) {
+export default function SixWeekComponent({sixWeekData, sixWeekIndex}) {
   return (
+    <>
+      <EmployeeName>
+        Mikko Tirronen
+      </EmployeeName>
       <Container>
-          {console.log(week)}
-       {week.map((item) => {
-        return <WeekComponent data={item.data}></WeekComponent>;
-      })}
+       {sixWeekData.map((item,index) => {
+         return <WeekComponent key={Math.random()} weekData={item.data} sixWeekIndex={sixWeekIndex}></WeekComponent>;
+        })}
     </Container>
+        </>
     );
 }
 const Container = styled.div`
@@ -18,7 +22,15 @@ const Container = styled.div`
   margin: auto;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
   width: 90.7vw;
-  //height: 90.3vh;
   grid-column-gap: 1vw;
   grid-row-gap: 1vh;
 `;
+const EmployeeName = styled.div`
+border-style: solid;
+min-width: 90%;
+font-size: 1vw;
+text-align: right;
+padding-right: 1%;
+height: 98%;
+font-weight: 600;
+`
