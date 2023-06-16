b643f0c4d5df119fc6a8f8eccf3066c78b4bbfb2
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-06-16 21:08:13 +0200

    created settings page

--- src/components/Pages/SettingsPageComponent.jsx
+++ src/components/Pages/SettingsPageComponent.jsx
@@ -1,221 +1,108 @@
 import React from "react";
 import HeaderComponent from "../HeaderComponent";
-import SixWeekComponent from "../SixWeekComponent";
-import { getEmployeeData } from "./HomePageComponent";
+
 import styled from "styled-components";
+import { shiftColors } from "../DayComponent";
 
 export default function SettingsPageComponent() {
-  let data = [
-    [
-      "*Hus 52 VNS",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-    ],
-    [
-      "",
-      "Vecka 2 Januari",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "Vecka 3 Januari",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "Vecka 4 Januari",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "Vecka 5 Januari/Februari",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "Vecka 6 Februari",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "Vecka 7 Februari",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-    ],
-    [
-      "",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-      "24",
-      "25",
-      "26",
-      "27",
-      "28",
-      "29",
-      "30",
-      "31",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-    ],
-    
-    [
-      "Namn",
-      "MÅ",
-      "TI",
-      "ON",
-      "TO",
-      "FR",
-      "LÖ",
-      "SÖ",
-      "MÅ",
-      "TI",
-      "ON",
-      "TO",
-      "FR",
-      "LÖ",
-      "SÖ",
-      "MÅ",
-      "TI",
-      "ON",
-      "TO",
-      "FR",
-      "LÖ",
-      "SÖ",
-      "MÅ",
-      "TI",
-      "ON",
-      "TO",
-      "FR",
-      "LÖ",
-      "SÖ",
-      "MÅ",
-      "TI",
-      "ON",
-      "TO",
-      "FR",
-      "LÖ",
-      "SÖ",
-      "MÅ",
-      "TI",
-      "ON",
-      "TO",
-      "FR",
-      "LÖ",
-      "SÖ",
-    ],
-  ];
-
+ 
   return (
     <OuterWrapper>
-      <HeaderComponent header={"Settings"}/>
+      <HeaderComponent header={"Settings"} />
+
       <Container>
-        {data.map((item) => {
+        {shiftColors.map((item) => (
+          <>
+            <InnerWrapper>
+              <ColorHeader>Color: {item.color}</ColorHeader>
+              <ColorAndListContainer>
+                <ColorContainer inputColor={item.color}></ColorContainer>
+                <ListContainer>
+                  <List>
+                    {item.shift.map((item) => (
+                      <ListItemContainer>
+                        <li>{item} </li>
+                        <span>
+                          -<DeleteButton>Delete</DeleteButton>
+                        </span>
+                      </ListItemContainer>
+                    ))}
+                  </List>
+                </ListContainer>
+              </ColorAndListContainer>
+            </InnerWrapper>
+          </>
+        ))}
+        {/* {data.map((item) => {
           if (item[0].charAt(0) === "*") {
             return <SectionHeader>{item[0]}</SectionHeader>
           }
             return <SixWeekComponent sixWeekData={getEmployeeData(item)} />;
-        })}
+        })} */}
       </Container>
     </OuterWrapper>
   );
 }
+const ListItemContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  margin-top: 5px;
+  padding-right: 3vh;
+`;
+const DeleteButton = styled.button`
+  background-color: red;
+  font-weight: 600;
+  margin-left: 10px;
+  color: whitesmoke;
+  width: 80%;
+`;
+const ColorHeader = styled.h4`
+  margin: 0%;
+  padding: 0%;
+  text-align: center;
+`;
+const ColorAndListContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  padding-left: 3vh;
+`;
+const InnerWrapper = styled.div`
+  border: solid;
+  grid-column-start: 2;
+  grid-column-end: 3;
+  display: grid;
+  grid-template-rows: 1fr 3fr;
+  border-radius: 15px;
+  padding-top: 1vh;
+  padding-bottom: 2vh;
+  background-color: #a5cff5;
+`;
+const ListContainer = styled.div`
+  padding-left: 2vw;
+`;
+const List = styled.ul`
+  font-weight: 600;
+  margin: 0%;
+  list-style-type: none;
+`;
+const ColorContainer = styled.div`
+  width: 10vw;
+  height: 10vw;
+  background-color: ${(props) => props.inputColor || "whitesmoke"};
+  border: solid;
+`;
 
 const Container = styled.div`
+  margin-top: 1vh;
+  width: 100vw;
   display: grid;
+  grid-template-columns: 2fr 3fr 2fr;
+  grid-row-gap: 1vh;
+  font-size: 1.5vw;
+`;
 
-`;
-const HeaderContainer = styled.div`
-  text-align: center;
-  width: 100%;
-`;
 const OuterWrapper = styled.div`
   width: 100vw;
   display: block;
-`;
-const SectionHeader = styled.div`
-  
+  background-color: darkgray;
 `;
